6989fbffe0a345bfee2c67fe1abe9376cadc8b97
Corey Richardson <coreycr@icloud.com>
2021-03-23 08:57:52 -0500

    Created basic splash page

--- website/views.py
+++ website/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Pingo!")
+    return render(request, 'website/splash.html', {})
