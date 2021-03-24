e32edbf408434d8010690b7051e135f39cd6424f
Corey Richardson <coreycr@icloud.com>
2021-03-23 20:35:09 -0500

    Set root URL path to website app

--- coreyguitar/urls.py
+++ coreyguitar/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('website/', include('website.urls')),
+    path('', include('website.urls')),
     path('admin/', admin.site.urls),
 ]
