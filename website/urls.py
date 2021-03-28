85ad39022f10ce8af8c732592c66b93e1e61dcfc
Corey Richardson <coreycr@icloud.com>
2021-03-28 10:58:57 -0500

    Created base.html

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'website'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/splash.html', {})
+
+def home(request):
+    return render(request, 'website/home.html', {})
