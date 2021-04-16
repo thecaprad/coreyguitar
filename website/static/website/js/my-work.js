7bef4d1cac1deb5f466439bd89f22db03e4f5c23
Corey Richardson <coreycr@icloud.com>
2021-04-16 14:20:47 -0500

    Got my-work page links working so that clicking on a sample opens up a unique modal. Still need to fill each of the 9 modals with video links and info.

--- website/static/website/js/my-work.js
+++ website/static/website/js/my-work.js
@@ -1,10 +1,39 @@
+function handleWorkClick(i) {
+    document.getElementById('overlay').classList.toggle('active');
+    var marquee = document.getElementById('marquee');
+    marquee.classList.toggle('active');
+    turnOffMarqueeChildren();
+    // turn on work sample with matching id
+    marquee.children[i].style.display = "block";
+}
+
+function turnOffMarquee() {
+    document.getElementById('overlay').classList.toggle('active');
+    document.getElementById('marquee').classList.toggle('active');
+    turnOffMarqueeChildren();
+}
+
+function turnOffMarqueeChildren() {
+    for (it = 0; it < marquee.children.length; it++) {
+        marquee.children[it].style.display = "none";
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('work-1').addEventListener('click', function() {
-        document.getElementById('overlay').classList.toggle('active');
-        document.getElementsByClassName('marquee')[0].classList.toggle('active');
-    })
-    document.getElementsByClassName('x')[0].addEventListener('click', function() {
-        document.getElementById('overlay').classList.toggle('active');
-        document.getElementsByClassName('marquee')[0].classList.toggle('active');
-    })
+    var workEls = document.getElementsByClassName('worksamplebut');
+
+    for (let i = 0; i < workEls.length; i++) {
+        workEls[i].addEventListener('click', function() {
+            handleWorkClick(i);
+        })
+    }
+
+    var xEls = document.getElementsByClassName('x');
+    for (let i = 0; i < workEls.length; i++) {
+        xEls[i].addEventListener('click', function() {
+            document.getElementById('overlay').classList.toggle('active');
+            document.getElementById('marquee').classList.toggle('active');
+            allMarqueeChildrenOff();
+        })
+    }
 });
