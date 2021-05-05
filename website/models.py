5bce2471c0b5f88fde2f907284fcff9ff9a548d5
Corey Richardson <coreycr@icloud.com>
2021-05-05 11:19:16 -0500

    Created BlogEntry model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BlogEntry(models.Model):
+    pub_date = models.DateTimeField(null=True) # Null for drafts.
+    byline = models.CharField(max_length=100)
+    title = models.TextField()
+    content = models.TextField()
+    abstract = models.TextField()
+    is_draft = models.BooleanField()
