5e462ab0fa49d4f54155f369e030806a4c8f0f7f
Corey Richardson <coreycr@icloud.com>
2021-05-11 18:10:51 -0500

    Added basic blog entry view, template and URL

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ app_name = 'website'
 urlpatterns = [
     path('', views.index, name='index'),
     path('my-work/', views.my_work, name='my_work'),
+    path('blog/<int:id>/', views.blog_entry_detail, name='blog_entry_detail')
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogEntry
 
 def splash(request):
     return render(request, 'website/splash.html', {})
@@ -9,3 +10,7 @@ def index(request):
 
 def my_work(request):
     return render(request, 'website/my-work.html', {})
+
+def blog_entry_detail(request, id):
+    entry = get_object_or_404(BlogEntry, id=id)
+    return render(request, 'website/entry.html', {'entry': entry})
