cf9040cac076338aa37fc8fc6fd60a5cb6100de2
Corey Richardson <coreycr@icloud.com>
2021-06-07 13:11:07 -0500

    Removed old IP address from local ALLOWED_HOSTS

--- coreyguitar/settings.py
+++ coreyguitar/settings.py
@@ -9,7 +9,7 @@ with open(fp) as f:
 
 if platform.node() == "Coreys-MacBook-Air.local":
     DEBUG = True
-    ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.2']
+    ALLOWED_HOSTS = ['127.0.0.1']
 else:
     DEBUG = False
     ALLOWED_HOSTS = ['.pythonanywhere.com', '.coreyguitar.com']
