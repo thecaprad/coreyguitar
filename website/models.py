e50f11d8bd81fbe878dfff1d6fa8e0768f93e791
Corey <coreycr@hey.com>
2022-01-04 09:56:17 -0600

    Simplified BlogEntry.date to DateField rather than DateTimeField

--- website/models.py
+++ website/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class BlogEntry(models.Model):
-    pub_date = models.DateTimeField(null=True) # Null for drafts.
+    date = models.DateField(null=True, blank=True)
     byline = models.CharField(max_length=100)
-    title = models.TextField()
+    title = models.CharField(max_length=200)
     content = models.TextField()
     abstract = models.TextField()
     is_draft = models.BooleanField()
