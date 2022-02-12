fdf2dc78b941742f897e8bc1345551b137a7544e
Corey <coreycr@hey.com>
2022-02-12 16:22:15 -0600

    Creates Show and Venue models

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import BlogEntry
+from .models import BlogEntry, Show, Venue
 
 admin.site.register(BlogEntry)
+admin.site.register(Show)
+admin.site.register(Venue)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 class BlogEntry(models.Model):
@@ -12,3 +13,29 @@ class BlogEntry(models.Model):
     @property
     def url(self):
         return f'/blog/{self.id}/{self.slug}/'
+
+class Venue(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    street = models.CharField(null=False, blank=False, max_length=50)
+    city = models.CharField(null=False, blank=False, max_length=20)
+    state = models.CharField(null=False, blank=False, max_length=20)
+    url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return "{} — {}, {}".format(
+            self.name,
+            self.city,
+            self.state
+        )
+
+class Show(models.Model):
+    date = models.DateTimeField(null=False, blank=False)
+    description = models.CharField(null=False, blank=False, max_length=100)
+    venue = models.ForeignKey(Venue, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} — {} @ {}".format(
+            self.date.strftime('%x'),
+            self.description,
+            self.venue.name
+        )
